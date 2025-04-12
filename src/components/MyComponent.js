b93cd9d39abf83e9fd6fafb006972220979e7706
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-04-12 09:06:01 +0700

    Nesting Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,65 +1,13 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 
 class MyComponent extends React.Component {
-  state = {
-    name: 'Quan xau trai',
-    address: 'Can Tho',
-    age: 21,
-    position: {
-      X: 0,
-      Y: 0,
-    }
-  }
-
-  handleClick(event) {
-    // alert('CLick me!');
-    console.log(event);
-    this.setState({
-      name: 'QuanAP',
-      age: Math.floor(Math.random() * 100),
-    })
-  }
-
-  handlePosition = (event) => {
-    this.setState({
-      position: {
-        X: event.screenX,
-        Y: event.screenY,
-      }
-    })
-    console.log(">>> Position mouse: " + "X: " + event.screenX + " - Y:" + event.screenY);
-  }
-
-  handleInput = (event) => {
-    this.setState({
-      name: event.target.value,
-    })
-  }
-
-  handleSubmitForm = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  }
 
   render() {
     return (
-      <>
-        <div>
-          <div>
-            My name is {this.state.name} and I'm from {this.state.address}. I'm {this.state.age}
-          </div>
-          <div>
-            <button onClick={(event) => this.handleClick(event)}>Click here</button>
-          </div>
-          <div>
-            <span>Position Mouse on Screen: X: {this.state.position.X} - Y: {this.state.position.Y}</span>
-          </div>
-          <form onSubmit={(event) => this.handleSubmitForm(event)}>
-            <input type="text" onChange={(event) => this.handleInput(event)} />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      </>
+      <div>
+        <UserInfo />
+      </div>
     )
   }
 }
