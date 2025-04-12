c675b9fd8bf1ddf0c2839ac04826ad8b6f115593
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-04-12 08:15:43 +0700

    DOM event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,13 +5,42 @@ class MyComponent extends React.Component {
     name: 'Quan xau trai',
     address: 'Can Tho',
     age: 21,
+    position: {
+      X: 0,
+      Y: 0,
+    }
+  }
+
+  handleClick(event) {
+    // alert('CLick me!');
+    console.log(event);
+  }
+
+  handlePosition = (event) => {
+    this.setState({
+      position: {
+        X: event.screenX,
+        Y: event.screenY,
+      }
+    })
+    console.log(">>> Position mouse: " + "X: " + event.screenX + " - Y:" + event.screenY);
   }
 
   render() {
     return (
-      <div>
-        My name is {this.state.name} and I'm from {this.state.address}
-      </div>
+      <>
+        <div>
+          <div>
+            My name is {this.state.name} and I'm from {this.state.address}
+          </div>
+          <div>
+            <button onClick={this.handleClick} onMouseOver={this.handlePosition}>Click here</button>
+          </div>
+          <div>
+            <span>Position Mouse on Screen: X: {this.state.position.X} - Y: {this.state.position.Y}</span>
+          </div>
+        </div>
+      </>
     )
   }
 }
